c34361bfffd70063e802f2528510d55e5980071c
Andrew Apicello <andrew.apicello@gmail.com>
2025-03-11 17:44:12 -0400

    instantiate temperature calculator

---
+++ OtherTemperatureCalculator.js
@@ -0,0 +1,22 @@
+var farenheit = 75;
+var celsius = Math.ceil(((farenheit - 32) * 5) / 9);
+var kelvin = celsius + 273.15;
+
+if (farenheit == 1) {
+	console.log(
+		farenheit + " degree Farenheit is " + celsius + " degrees Celsius"
+	);
+	console.log(farenheit + " degree Farenheit is " + kelvin + " Kelvin ");
+} else {
+	if (celsius == 1) {
+		console.log(
+			farenheit + " degrees Farenheit is " + celsius + " degree Celsius"
+		);
+	} else {
+		console.log(
+			farenheit + " degrees Farenheit is " + celsius + " degrees Celsius"
+		);
+	}
+
+	console.log(farenheit + " degrees Farenheit is " + kelvin + " Kelvin ");
+}
