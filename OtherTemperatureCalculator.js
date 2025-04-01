7df73a2d3807cc70b2801065f7634c14f4147e6f
Andrew Apicello <andrewapicello@sentauriai.com>
2025-04-01 19:04:01 -0400

    created new variables to display new text

--- OtherTemperatureCalculator.js
+++ OtherTemperatureCalculator.js
@@ -4,22 +4,27 @@ function calculateTemperatureFromFarenheit() {
 	var celsius = Math.ceil(((farenheit - 32) * 5) / 9);
 	var kelvin = celsius + 273.15;
 
+	var celsiusStatement;
+	var kelvinStatement;
+
 	if (farenheit == 1) {
-		console.log(
-			farenheit + " degree Farenheit is " + celsius + " degrees Celsius"
-		);
-		console.log(farenheit + " degree Farenheit is " + kelvin + " Kelvin ");
+		celsiusStatement =
+			farenheit + " degree Farenheit is " + celsius + " degrees Celsius";
+
+		kelvinStatement = farenheit + " degree Farenheit is " + kelvin + " Kelvin";
 	} else {
 		if (celsius == 1) {
-			console.log(
-				farenheit + " degrees Farenheit is " + celsius + " degree Celsius"
-			);
+			celsiusStatement =
+				farenheit + " degrees Farenheit is " + celsius + " degree Celsius";
 		} else {
-			console.log(
-				farenheit + " degrees Farenheit is " + celsius + " degrees Celsius"
-			);
+			celsiusStatement =
+				farenheit + " degrees Farenheit is " + celsius + " degrees Celsius";
 		}
 
-		console.log(farenheit + " degrees Farenheit is " + kelvin + " Kelvin ");
+		kelvinStatement =
+			farenheit + " degrees Farenheit is " + kelvin + " Kelvin ";
+
+		document.getElementById("celsius").innerHTML = celsiusStatement;
+		document.getElementById("kelvin").innerHTML = kelvinStatement;
 	}
 }
