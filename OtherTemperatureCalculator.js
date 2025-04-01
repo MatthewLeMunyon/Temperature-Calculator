4c9360840781fa747b19dba4d8c4d68e7aa46dc9
Andrew Apicello <andrewapicello@sentauriai.com>
2025-04-01 18:07:05 -0400

    my first commit

--- OtherTemperatureCalculator.js
+++ OtherTemperatureCalculator.js
@@ -1,22 +1,25 @@
-var farenheit = 75;
-var celsius = Math.ceil(((farenheit - 32) * 5) / 9);
-var kelvin = celsius + 273.15;
+function calculateTemperatureFromFarenheit() {
+	var farenheit = document.getElementById("farenheit").value;
 
-if (farenheit == 1) {
-	console.log(
-		farenheit + " degree Farenheit is " + celsius + " degrees Celsius"
-	);
-	console.log(farenheit + " degree Farenheit is " + kelvin + " Kelvin ");
-} else {
-	if (celsius == 1) {
+	var celsius = Math.ceil(((farenheit - 32) * 5) / 9);
+	var kelvin = celsius + 273.15;
+
+	if (farenheit == 1) {
 		console.log(
-			farenheit + " degrees Farenheit is " + celsius + " degree Celsius"
+			farenheit + " degree Farenheit is " + celsius + " degrees Celsius"
 		);
+		console.log(farenheit + " degree Farenheit is " + kelvin + " Kelvin ");
 	} else {
-		console.log(
-			farenheit + " degrees Farenheit is " + celsius + " degrees Celsius"
-		);
-	}
+		if (celsius == 1) {
+			console.log(
+				farenheit + " degrees Farenheit is " + celsius + " degree Celsius"
+			);
+		} else {
+			console.log(
+				farenheit + " degrees Farenheit is " + celsius + " degrees Celsius"
+			);
+		}
 
-	console.log(farenheit + " degrees Farenheit is " + kelvin + " Kelvin ");
+		console.log(farenheit + " degrees Farenheit is " + kelvin + " Kelvin ");
+	}
 }
